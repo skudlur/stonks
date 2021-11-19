ef524691cd66a85753aade8263e98ae513e4d16e
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2021-11-19 20:05:04 +0530

    added alerts (does not run in bg tho)

--- stonks.py
+++ stonks.py
@@ -1,7 +1,7 @@
 import discord
 import os
 from wazirx_sapi_client.rest import Client
-import shib
+import mod
 from discord.ext import commands
 
 wazirClient = Client()
@@ -17,10 +17,11 @@ async def test(mes):
 
 @bot.command()
 async def price(com, cur, ticker):
-    await com.channel.send(shib.tickerPrice(cur, ticker))
+    await com.channel.send(mod.tickerPrice(cur, ticker))
 
-# @bot.command()
-# async def alert(com, cur, ticker, alertPrice):
+@bot.command()
+async def alert(com, cur, ticker, alertPrice):
+    await com.channel.send(mod.PriceBreakoutAlert(cur, ticker, alertPrice))
 
 # client.run(os.getenv('TOKEN'))
 bot.run((os.getenv('TOKEN')))
