af9b88b960d2f4f1f5a9640aa131545328943fdb
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2021-11-19 19:53:03 +0530

    added mod.py

---
+++ mod.py
@@ -0,0 +1,28 @@
+from wazirx_sapi_client.rest import Client
+import time
+import os
+
+wazirClient = Client()
+wazirClient = Client(api_key=os.getenv('api_key'), secret_key=os.getenv('secret_key'))
+
+def shibPrice():
+    shibData = wazirClient.send("historical_trades",
+                {"limit": 10, "symbol": "shibinr", "recvWindow": 10000, "timestamp": int(time.time() * 1000)}
+                )
+
+    shibData_parsed = shibData[1][1]
+    shibPriceRec = shibData_parsed.get("price")
+    return shibPriceRec
+
+def currencyTickerLowercase(currency, ticker):
+    return ticker.lower() + currency.lower()
+
+def tickerPrice(currency, ticker):
+    lcTicker = currencyTickerLowercase(currency, ticker)
+    tickerData = wazirClient.send("historical_trades",
+                {"limit": 10, "symbol": lcTicker, "recvWindow": 10000, "timestamp": int(time.time() * 1000)}
+                )
+    
+    tickerPrice_parsed = tickerData[1][1]
+    tickerPriceRec = tickerPrice_parsed.get("price")
+    return tickerPriceRec
