13a3f72838fbcba885af0d28e1fe3fe52a9c1428
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2021-11-20 13:03:14 +0530

    alerts still under wip

--- mod.py
+++ mod.py
@@ -5,6 +5,8 @@ import os
 wazirClient = Client()
 wazirClient = Client(api_key=os.getenv('api_key'), secret_key=os.getenv('secret_key'))
 
+alertList = []
+
 def shibPrice():
     shibData = wazirClient.send("historical_trades",
                 {"limit": 10, "symbol": "shibinr", "recvWindow": 10000, "timestamp": int(time.time() * 1000)}
@@ -37,3 +39,40 @@ def PriceBreakoutAlert(currency, ticker, alertPrice):
             ucCurrency = currency.upper()
             ucTicker = ticker.upper()
             return f"{ucTicker}({ucCurrency}) has hit {alertPrice}!"
+
+def PriceBreakoutAlertAdd(currency, ticker, alertPrice):
+    alertListing = [currency, ticker, alertPrice]
+    alertList.append(alertListing)
+    print(alertList)
+    return "Alert successfully added!"
+
+def PriceBreakoutAlertOut():
+    for i in alertList:
+        iList = list(i)
+        # print(tickerPrice(iList[0], iList[1]))
+        # print(iList[2])
+        if (float(iList[2]) <= float(tickerPrice(iList[0], iList[1]))):
+            return True, i
+            break
+        else:
+            return False
+            # ucCurrency = i[0].upper()
+            # ucTicker = i[1].upper()
+            # alertPrice = i[2]
+            # return f"{ucTicker}({ucCurrency}) has hit {alertPrice}!"
+            # break
+
+def PriceBreakoutCheckingLoop():
+    # print(PriceBreakoutAlertOut())
+    breakoutData = PriceBreakoutAlertOut()
+    outTrueFalse = breakoutData[0]
+    print(outTrueFalse)
+    if (outTrueFalse):
+        ucCurrency = breakoutData[1][0].upper()
+        ucTicker = breakoutData[1][1].upper()
+        alertPrice = breakoutData[1][2]
+        return f"{ucTicker}({ucCurrency}) has hit {alertPrice}!"
+    else:
+        time.sleep(5)
+        PriceBreakoutCheckingLoop()
+    
