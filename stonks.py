1e7a11c5f7765fca6b36770604c5958c0b1e629a
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2021-11-20 13:02:33 +0530

    made changes to alerts
    but it is still in work-in-progress

--- stonks.py
+++ stonks.py
@@ -1,5 +1,7 @@
 import discord
 import os
+
+from discord import channel
 from wazirx_sapi_client.rest import Client
 import mod
 from discord.ext import commands
@@ -21,7 +23,10 @@ async def price(com, cur, ticker):
 
 @bot.command()
 async def alert(com, cur, ticker, alertPrice):
-    await com.channel.send(mod.PriceBreakoutAlert(cur, ticker, alertPrice))
+    await com.channel.send(mod.PriceBreakoutAlertAdd(cur, ticker, alertPrice))
+    mod.PriceBreakoutCheckingLoop()
+
+print(mod.alertList)
 
 # client.run(os.getenv('TOKEN'))
-bot.run((os.getenv('TOKEN')))
+bot.run((os.getenv('DISC_TOKEN')))
